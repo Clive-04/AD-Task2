9396c7be09ea007c20f291cd644f7d84c3836fab
Clive-04 <202310384@fit.edu.ph>
2025-05-18 20:12:24 +0800

    fix: modified the handler.php and added filters

--- handlers/starterDex.handler.php
+++ handlers/starterDex.handler.php
@@ -1,18 +1,24 @@
 <?php
     require_once(__DIR__ . '/../utils/StarterDex.util.php');
 
-    function renderStarterList(){
+    function renderStarterList($filter = "All"){
         $starters = getStarterPokemon();
         foreach ($starters as $generation => $pokemonList) {
-            echo "<h2>$generation</h2><div class='starter-container'>";
-            foreach ($pokemonList as $pokemon) {
-                echo "<div class='starter-card'>";
-                echo "<img src='../../" . $pokemon['image'] . "'alt='" . $pokemon['name'] . "'/>";
-                echo "<h3>" . $pokemon['name'] . "</h3>";
-                echo "<p>Type: " . $pokemon['type'] . "</p>";
+            $filteredList = array_filter($pokemonList, function($poke) use ($filter){
+                return $filter === "All" || $poke['type'] === $filter;
+            });
+
+            if (count($filteredList) > 0){
+                echo "<h2>$generation</h2><div class='starter-container'>";
+                foreach ($filteredList as $pokemon) {
+                    echo "<div class='starter-card'>";
+                    echo "<img src='../../" . $pokemon['image'] . "'alt='" . $pokemon['name'] . "'/>";
+                    echo "<h3>" . $pokemon['name'] . "</h3>";
+                    echo "<p>Type: " . $pokemon['type'] . "</p>";
+                    echo "</div>";
+                }
                 echo "</div>";
             }
-            echo "</div>";
         }
     }
 ?>
