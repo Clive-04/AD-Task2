a4b84c39c8751c4fd5eaaeced47d804811700eec
Clive-04 <202310384@fit.edu.ph>
2025-05-18 19:42:53 +0800

    feat: added handler for starterDex

---
+++ handlers/starterDex.handler.php
@@ -0,0 +1,18 @@
+<?php
+    require_once(__DIR__ . '/utils/StarterDex.util.php');
+
+    function renderStarterList(){
+        $starters = getStarterPokemon();
+        foreach ($starters as $generation => $pokemonList) {
+            echo "<h2>$generation</h2><div class='starter-container'>";
+            foreach ($pokemonList as $pokemon) {
+                echo "<div class='starter-card'>";
+                echo "<img src='../../" . $pokemon['image'] . "'alt='" . $pokemon['name'] . "'/>";
+                echo "<h3>" . $pokemon['name'] . "</h3>";
+                echo "<p>Type: " . $pokemon['type'] . "</p>";
+                echo "</div>";
+            }
+            echo "</div>";
+        }
+    }
+?>
