d4c6d31f22429dac47b1299fd67f850d117b3ccf
Clive-04 <202310384@fit.edu.ph>
2025-05-18 20:57:01 +0800

    feat: added more css code for the global

--- components/navbar.component.php
+++ components/navbar.component.php
@@ -1,6 +1,6 @@
-<nav>
-    <ul>
+<nav class="navbar">
+    <ul class="navbar-list">
         <li><a href="/index.php">Home</a></li>
-        <li><a href="/../page/starterList/index.php">Starter Dex</a></li>
+        <li><a href="/page/starterList/index.php">Starter Dex</a></li>
     </ul>
 </nav>
