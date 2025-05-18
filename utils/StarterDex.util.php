04fe5f563419a46854610498f82d553eee6c8a7a
Clive-04 <202310384@fit.edu.ph>
2025-05-18 21:23:42 +0800

    feat: added more images

--- utils/StarterDex.util.php
+++ utils/StarterDex.util.php
@@ -4,32 +4,32 @@ function getStarterPokemon() {
     return [
         "Gen 1" => [
             ["name" => "Bulbasaur", "type" => ["Grass", "Poison"], "image" => "assets/img/bulbasaur.png"],
-            ["name" => "Charmander", "type" => ["Fire"], "image" => "assets/img/charmander.jpg"],
-            ["name" => "Squirtle", "type" => ["Water"], "image" => "assets/img/squirtle.jpg"]
+            ["name" => "Charmander", "type" => ["Fire"], "image" => "assets/img/charmander.png"],
+            ["name" => "Squirtle", "type" => ["Water"], "image" => "assets/img/squirtle.png"]
         ],
 
         "Gen 2" => [
-            ["name" => "Chikorita", "type" => ["Grass"], "image" => "assets/img/chikorita.jpg"],
-            ["name" => "Cyndaquil", "type" => ["Fire"], "image" => "assets/img/cyndaquil.jpg"],
-            ["name" => "Totodile", "type" => ["Water"], "image" => "assets/img/totodile.jpg"]
+            ["name" => "Chikorita", "type" => ["Grass"], "image" => "assets/img/chikorita.png"],
+            ["name" => "Cyndaquil", "type" => ["Fire"], "image" => "assets/img/cyndaquil.png"],
+            ["name" => "Totodile", "type" => ["Water"], "image" => "assets/img/totodile.png"]
         ],
 
         "Gen 3" => [
-            ["name" => "Treecko", "type" => ["Grass"], "image" => "assets/img/treecko.jpg"],
-            ["name" => "Torchic", "type" => ["Fire"], "image" => "assets/img/torchi.jpg"],
-            ["name" => "Mudkip", "type" => ["Water"], "image" => "assets/img/mudkip.jpg"]
+            ["name" => "Treecko", "type" => ["Grass"], "image" => "assets/img/treecko.png"],
+            ["name" => "Torchic", "type" => ["Fire"], "image" => "assets/img/torchi.png"],
+            ["name" => "Mudkip", "type" => ["Water"], "image" => "assets/img/mudkip.png"]
         ],
 
         "Gen 4" => [
-            ["name" => "Turtwig", "type" => ["Grass"], "image" => "assets/img/turtwig.jpg"],
-            ["name" => "Chimchar", "type" => ["Fire"], "image" => "assets/img/chimchar.jpg"],
-            ["name" => "Piplup", "type" => ["Water"], "image" => "assets/img/piplup.jpg"]
+            ["name" => "Turtwig", "type" => ["Grass"], "image" => "assets/img/turtwig.png"],
+            ["name" => "Chimchar", "type" => ["Fire"], "image" => "assets/img/chimchar.png"],
+            ["name" => "Piplup", "type" => ["Water"], "image" => "assets/img/piplup.png"]
         ],
 
         "Gen 5" => [
-            ["name" => "Snivy", "type" => ["Grass"], "image" => "assets/img/snivy.jpg"],
-            ["name" => "Tepig", "type" => ["Fire"], "image" => "assets/img/tepig.jpg"],
-            ["name" => "Oshawott", "type" => ["Water"], "image" => "assets/img/oshawott.jpg"]
+            ["name" => "Snivy", "type" => ["Grass"], "image" => "assets/img/snivy.png"],
+            ["name" => "Tepig", "type" => ["Fire"], "image" => "assets/img/tepig.png"],
+            ["name" => "Oshawott", "type" => ["Water"], "image" => "assets/img/oshawott.png"]
         ]
     ];
 }
