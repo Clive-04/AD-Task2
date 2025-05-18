b1c0d5bc31db65c7c75f28d5fbf8e07e5f387588
Clive-04 <202310384@fit.edu.ph>
2025-05-18 20:07:48 +0800

    feat: added 3 more generations for more content

--- utils/StarterDex.util.php
+++ utils/StarterDex.util.php
@@ -12,6 +12,24 @@ function getStarterPokemon() {
             ["name" => "Chikorita", "type" => "Grass", "image" => "assets/img/chikorita.jpg"],
             ["name" => "Cyndaquil", "type" => "Fire", "image" => "assets/img/cyndaquil.jpg"],
             ["name" => "Totodile", "type" => "Water", "image" => "assets/img/totodile.jpg"]
+        ],
+
+        "Gen 3" => [
+            ["name" => "Treecko", "type" => "Grass", "image" => "assets/img/treecko.jpg"],
+            ["name" => "Torchi", "type" => "Fire", "image" => "assets/img/torchi.jpg"],
+            ["name" => "Mudkip", "type" => "Water", "image" => "assets/img/mudkip.jpg"]
+        ],
+
+        "Gen 4" => [
+            ["name" => "Turtwig", "type" => "Grass", "image" => "assets/img/turtwig.jpg"],
+            ["name" => "Chimchar", "type" => "Fire", "image" => "assets/img/chimchar.jpg"],
+            ["name" => "Piplup", "type" => "Water", "image" => "assets/img/piplup.jpg"]
+        ],
+
+        "Gen 5" => [
+            ["name" => "Snivy", "type" => "Grass", "image" => "assets/img/snivy.jpg"],
+            ["name" => "Tepig", "type" => "Fire", "image" => "assets/img/tepig.jpg"],
+            ["name" => "Oshawott", "type" => "Water", "image" => "assets/img/oshawott.jpg"]
         ]
     ];
 }
