9caedba8c49969fbe3fede768c2f424476f5f108
Clive-04 <202310384@fit.edu.ph>
2025-05-18 19:34:35 +0800

    feat: added starterdex util

---
+++ utils/StarterDex.util.php
@@ -0,0 +1,18 @@
+<?php
+
+function getStarterPokemon() {
+    return [
+        "Gen 1" => [
+            ["name" => "Bulbasaur", "type" => "Grass/Poison", "image" => "assets/img/bulbasaur.jpg"],
+            ["name" => "Charmander", "type" => "Fire", "image" => "assets/img/charmander.jpg"],
+            ["name" => "Squirtle", "type" => "Water", "image" => "assets/img/squirtle.jpg"]
+        ],
+
+        "Gen 2" => [
+            ["name" => "Chikorita", "type" => "Grass", "image" => "assets/img/chikorita.jpg"],
+            ["name" => "Cyndaquil", "type" => "Fire", "image" => "assets/img/cyndaquil.jpg"],
+            ["name" => "Totodile", "type" => "Water", "image" => "assets/img/totodile.jpg"]
+        ]
+    ];
+}
+?>
