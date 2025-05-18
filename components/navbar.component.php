f7c12aa5e7821ac74c88d8ea8b7287b4b8e85e48
Clive-04 <202310384@fit.edu.ph>
2025-05-18 22:01:06 +0800

    feat: updated the code and added a logo for the navbar

--- components/navbar.component.php
+++ components/navbar.component.php
@@ -1,6 +1,11 @@
 <nav class="navbar">
-    <ul class="navbar-list">
-        <li><a href="/index.php">Home</a></li>
-        <li><a href="/page/starterList/index.php">Starter Dex</a></li>
-    </ul>
+    <div class="navbar-content">
+        <a href="/index.php" class="logo-link">
+            <img src="/assets/img/masterball.png" alt="Masterball Logo" class="logo">
+        </a>
+        <ul class="navbar-list">
+            <li><a href="/index.php">Home</a></li>
+            <li><a href="/page/starterList/index.php">Starter Dex</a></li>
+        </ul>
+    </div>
 </nav>
