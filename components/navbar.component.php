d546f6a381c4a6c906fd660e25fe73bac2a071c9
Clive-04 <202310384@fit.edu.ph>
2025-05-18 20:13:37 +0800

    fix: changed the href for the starter dex button

--- components/navbar.component.php
+++ components/navbar.component.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
         <li><a href="/index.php">Home</a></li>
-        <li><a href="/page/starterList">Starter Dex</a></li>
+        <li><a href="/../page/starterList/index.php">Starter Dex</a></li>
     </ul>
 </nav>
