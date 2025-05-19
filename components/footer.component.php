b5e49ec01fcf14022a21ff1a3da16f47ccbb5665
Clive-04 <202310384@fit.edu.ph>
2025-05-19 19:00:26 +0800

    feat: added the file structure and footer component

---
+++ components/footer.component.php
@@ -0,0 +1,10 @@
+<footer class="site-footer">
+    <div class="footer-container">
+        <p>&copy; <? date("Y") ?> Pokemon Starter Dex - AD-Task2. All Rights Reserved</p>
+        <p>
+            <a href="https://www.pokemon.com" target="_blank" rel="noopener noreferrer">
+                Visit Official Pokemon Site
+            </a>
+        </p>
+    </div>
+</footer>
